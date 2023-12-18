d562f83a00b222ed6ac5ce70615ea355a0c710af
ElectrixFox <lukecphillips2006@gmail.com>
2023-12-18 18:33:13 +0000

    Updates to the Template Layout

--- TemplateLayout.cpp
+++ TemplateLayout.cpp
@@ -2,13 +2,29 @@
 // #MENU LOOP FUNCTION
 int ID_MenuLoop()
 {
-int ID_choice;
+int ID_menustatus;
 
-while (ID_choice != ID_end)
+while (ID_menustatus != ID_end)
     {
-    ID_choice = ID_MenuFunction();
+    ID_menustatus = ID_MenuFunction();
     clrscr();
     }
 
 return 0;
+}
+
+// #MENU CASE FUNCTION
+int ID_MenuFunction()
+{
+int ID_choice;
+
+clrscr();
+
+// #OUTPUT MENU NAME
+// #OUTPUT CHOICES
+
+cout << "Enter Choice: ";
+cin >> ID_choice;
+
+return ID_choice;
 }
